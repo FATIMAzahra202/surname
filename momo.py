0d0fbf6f685fd42e54e30e0ad389f8e7d15032df
mghizlat idrissi <162041720+FATIMAzahra202@users.noreply.github.com>
2024-04-26 11:18:48 +0100

    Update momo.py

--- momo.py
+++ momo.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 def get_nationality(name):
     response = requests.get(f"https://api.nationalize.io?name={name}")
@@ -10,15 +11,30 @@ def get_nationality(name):
 
 def display_nationality(data):
     if data and 'country' in data:
+        results = []
         for country in data['country']:
+            result = {
+                "Pays": country['country_id'],
+                "Probabilité (%)": f"{country['probability']*100:.2f}"
+            }
+            results.append(result)
             st.write(f"Probabilité : {country['probability']*100:.2f}% que le nom soit originaire de {country['country_id']}")
+        return pd.DataFrame(results)
     else:
         st.write("Aucune donnée trouvée pour ce nom de famille.")
+        return pd.DataFrame()
 
 st.title('Devinez l’origine des noms de famille')
 
 name_input = st.text_input('Entrez un nom de famille:', '')
 
-if name_input:
-    data = get_nationality(name_input)
-    display_nationality(data)
+if st.button('Devinez l’origine'):
+    if name_input:
+        data = get_nationality(name_input)
+        result_df = display_nationality(data)
+        st.download_button(
+            label="Télécharger les données",
+            data=result_df.to_csv(index=False).encode('utf-8'),
+            file_name=f"{name_input}_origins.csv",
+            mime='text/csv',
+        )
